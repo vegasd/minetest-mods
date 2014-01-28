e7d6036e317181657111eeb1ccdf783044f20b27
Sullome <sullome@yandex.com>
2014-01-29 03:41:10 +0400

    Add base block and write README

--- black_brick/init.lua
+++ black_brick/init.lua
@@ -1,1 +1,7 @@
-
+minetest.register_node("black_brick:brick",{
+    description = "Black Brick",
+    tiles = {"black_brick.png"},
+	groups = {cracky=3, stone=1},
+    drop = "black_brick:brick",
+	sounds = default.node_sound_stone_defaults(),
+})
