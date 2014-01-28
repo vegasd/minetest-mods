606a3baa2416dafa8223120aa135e3fc5636570f
Sullome <sullome@yandex.com>
2014-01-29 03:46:03 +0400

    Add stair and slab. Add dependence on stairs mod.

--- black_brick/init.lua
+++ black_brick/init.lua
@@ -5,3 +5,11 @@ minetest.register_node("black_brick:brick",{
     drop = "black_brick:brick",
 	sounds = default.node_sound_stone_defaults(),
 })
+
+stairs.register_stair_and_slab("black_brick", "black_brick:brick",
+    {cracky=3},
+    {"black_brick.png"},
+    "Black Brick Stair",
+    "Black Brick Slab",
+    default.node_sound_stone_defaults()
+)
