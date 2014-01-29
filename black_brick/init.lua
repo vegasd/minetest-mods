5d46c4586f7efdca93d80df42c5ee592799d5b5c
Sullome <sullome@yandex.com>
2014-01-29 12:55:55 +0400

    Add black fence.

--- black_brick/init.lua
+++ black_brick/init.lua
@@ -6,6 +6,22 @@ minetest.register_node("black_brick:brick",{
 	sounds = default.node_sound_stone_defaults(),
 })
 
+minetest.register_node("black_brick:fence", {
+	description = "Black Fence",
+	drawtype = "fencelike",
+	tiles = {"black_brick.png"},
+	--inventory_image = "default_fence.png",
+	--wield_image = "default_fence.png",
+	paramtype = "light",
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
+	},
+	groups = {choppy=2,oddly_breakable_by_hand=2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
 stairs.register_stair_and_slab("black_brick", "black_brick:brick",
     {cracky=3},
     {"black_brick.png"},
