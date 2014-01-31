f808596813866ad2e1fcf79e763d7024b421d332
Sullome <sullome@yandex.com>
2014-02-01 03:01:23 +0400

    Testing text HUD.

--- new_chat/init.lua
+++ new_chat/init.lua
@@ -1,1 +1,16 @@
-
+minetest.register_on_chat_message(function(name,message)
+    minetest.get_player_by_name(name):hud_add({
+        hud_elem_type = "text",
+        text = message,
+        position = {x=0.05,y=0.9},
+        name = "chat",
+        scale = {x=500, y=50},
+        number = 0xFFFFFF,
+        item = 0,
+        direction = 0,
+        alignment = {x=0, y=0},
+        offset = {x=0, y=0}
+    })
+    
+    return true
+end)
