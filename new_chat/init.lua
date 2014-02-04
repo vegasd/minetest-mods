3ea6e57c9a1cf97796e450218476d3b66bcff6f1
Sullome <sullome@yandex.com>
2014-02-05 01:57:19 +0400

    Write HUD definition.

--- new_chat/init.lua
+++ new_chat/init.lua
@@ -1,48 +1,28 @@
-MESSAGES_ON_SCREEN=10
-LEFT_INDENT=0.05
+MESSAGES_ON_SCREEN = 10
+MAX_LENGTH         = 30
+LEFT_INDENT        = 0.00
+TOP_INDENT         = 0.92
+FONT_WIDTH         = 12
+FONT_HEIGHT        = 28
 
-minetest.register_on_chat_message(function(name,message)
-    local player = minetest.get_player_by_name(name)
-    local hud_id = player:hud_add({
-        hud_elem_type = "text",
-        text = message,
-        position = {x = LEFT_INDENT, y = 0.9},
-        name = "chat",
-        scale = {x=500, y=50},
-        number = 0xFFFFFF,
-        item = 0,
-        direction = 0,
-        alignment = {x=1, y=0},
-        offset = {x=0, y=0}
-    })
-    print("ADDED HUD: " .. hud_id)
-    
-    local start_id
-    if hud_id == MESSAGES_ON_SCREEN then start_id = 0
-    else start_id = hud_id + 1 end
-
-    print(end_id)
-
-    for id = start_id,hud_id+MESSAGES_ON_SCREEN+1 do
-        print("FOR id = " .. id)
-        if id >= MESSAGES_ON_SCREEN then
-            local real_id = id - MESSAGES_ON_SCREEN - 1
-
-            local hud = player:hud_get(real_id)
-            if hud ~= nil then 
-                print(dump(hud))
-                player:hud_change(real_id, position,
-                    {x = LEFT_INDENT, y = 0.5})
-                print(dump(hud))
-            end
-        else
-            local hud = player:hud_get(id)
-            if hud ~= nil then 
-                player:hud_change(id, position,
-                    {x = LEFT_INDENT, y = hud.position.y + 0.5/MESSAGES_ON_SCREEN})
-            end
-        end
+function createChatHUD(player)
+    for i=1,MESSAGES_ON_SCREEN do
+        local hud_id = player:hud_add({
+            hud_elem_type = "text",
+            text = i,
+            position = {x = LEFT_INDENT, y = TOP_INDENT},
+            name = "chat",
+            scale = {x=500, y=50},
+            number = 0xFFFFFF,
+            item = 0,
+            direction = 0,
+            alignment = {x=1, y=0},
+            offset = {x=FONT_WIDTH*16+4, y=-i*FONT_HEIGHT}
+        })
+        print("ADDED HUD: " .. hud_id)
     end
-    
-    return true
+end
+
+minetest.register_on_joinplayer(function(player)
+    minetest.after(1, createChatHUD, player)
 end)
