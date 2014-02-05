21a224041fdc29417708f9b5607772e092b60e4d
vegasd <nazghul@yandex.ru>
2014-02-05 14:20:04 +0400

    fixed wrong string splitting

--- kmchat/init.lua
+++ kmchat/init.lua
@@ -42,7 +42,7 @@ RANGE_WHISPER = 3
 
 GM_PREFIX = "[GM] "
 MESSAGES_ON_SCREEN = 10
-MAX_LENGTH         = 50
+MAX_LENGTH         = 100
 LEFT_INDENT        = 0.01
 TOP_INDENT         = 0.92
 FONT_WIDTH         = 12
@@ -180,9 +180,14 @@ minetest.register_on_chat_message(function(name, message)
         recieverpos = pls[i]:getpos()
         message = string.format(fmt, showname, submes)
         if math.sqrt((senderpos.x-recieverpos.x)^2 + (senderpos.y-recieverpos.y)^2 + (senderpos.z-recieverpos.z)^2) < range then
+            local splitter
             while message:len() > MAX_LENGTH do
-                addMessage(pls[i], name, message:sub(0,MAX_LENGTH), color)
-                message = message:sub(MAX_LENGTH+1)
+                splitter = string.find (message, " ", MAX_LENGTH)
+                if splitter == nil then
+                    splitter = MAX_LENGTH
+                end
+                addMessage(pls[i], name, message:sub(0,splitter), color)
+                message = message:sub(splitter+1)
             end
             addMessage(pls[i], name, message, color)
 
