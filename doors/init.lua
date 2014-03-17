b97db5095b8dc7204983f9b46956e7b3bfade140
hunterdelyx1 <hunterdelyx1@gmail.com>
2014-03-17 20:40:34 +0400

    door: sides' textures

--- doors/init.lua
+++ doors/init.lua
@@ -19,7 +19,8 @@ function doors:register_door(name, def)
 	def.groups.not_in_creative_inventory = 1
 	
 	local box = {{-0.5, -0.5, -0.5,   0.5, 0.5, -0.5+3/16}}
-	
+	--local box = {{-0.5, -0.5, -0.5,   0.5, 0.5, 0.5}}	
+
 	if not def.node_box_bottom then
 		def.node_box_bottom = box
 	end
@@ -129,7 +130,7 @@ function doors:register_door(name, def)
 	end
 	
 	minetest.register_node(name.."_b_1", {
-		tiles = {tb[2], tb[2], tb[2], tb[2], tb[1], tb[1].."^[transformfx"},
+		tiles = {tb[1], tb[3], tb[2], tb[2].."^[transformr180", tb[1], tb[1].."^[transformfx"},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		drop = name,
@@ -159,7 +160,7 @@ function doors:register_door(name, def)
 	})
 	
 	minetest.register_node(name.."_t_1", {
-		tiles = {tt[2], tt[2], tt[2], tt[2], tt[1], tt[1].."^[transformfx"},
+		tiles = {tt[3].."^[transformr180", tt[2], tt[2].."^[transformfx", tt[2].."^[transformr180", tt[1], tt[1].."^[transformfx"},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		drop = name,
@@ -189,7 +190,7 @@ function doors:register_door(name, def)
 	})
 	
 	minetest.register_node(name.."_b_2", {
-		tiles = {tb[2], tb[2], tb[2], tb[2], tb[1].."^[transformfx", tb[1]},
+		tiles = {tb[1], tb[3].."^[transformfy", tb[2].."^[transformfx", tb[2], tb[1].."^[transformfx", tb[1]},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		drop = name,
@@ -219,7 +220,7 @@ function doors:register_door(name, def)
 	})
 	
 	minetest.register_node(name.."_t_2", {
-		tiles = {tt[2], tt[2], tt[2], tt[2], tt[1].."^[transformfx", tt[1]},
+		tiles = {tt[3], tt[2], tt[2].."^[transformfx", tt[2], tt[1].."^[transformfx", tt[1]},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		drop = name,
@@ -254,8 +255,8 @@ doors:register_door("doors:door_wood", {
 	description = "Wooden Door",
 	inventory_image = "door_wood.png",
 	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=2,door=1},
-	tiles_bottom = {"door_wood_b.png", "door_brown.png"},
-	tiles_top = {"door_wood_a.png", "door_brown.png"},
+	tiles_bottom = {"door_wood_b.png", "door_wood_c.png","door_wood_d.png"},
+	tiles_top = {"door_wood_a.png", "door_wood_c.png","door_wood_d.png"},
 })
 
 minetest.register_craft({
@@ -271,8 +272,8 @@ doors:register_door("doors:door_steel", {
 	description = "Steel Door",
 	inventory_image = "door_steel.png",
 	groups = {snappy=1,bendy=2,cracky=1,melty=2,level=2,door=1},
-	tiles_bottom = {"door_steel_b.png", "door_grey.png"},
-	tiles_top = {"door_steel_a.png", "door_grey.png"},
+	tiles_bottom = {"door_steel_b.png", "door_steel_c.png", "door_steel_d.png"},
+	tiles_top = {"door_steel_a.png", "door_steel_c.png", "door_steel_d.png"},
 	only_placer_can_open = true,
 })
 
