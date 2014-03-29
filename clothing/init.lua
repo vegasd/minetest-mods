12d1a5cd3787d1e28eb76cb46f9e3811c36c8956
Sullome <sullome@yandex.com>
2014-03-29 23:19:31 +0400

    Add base functionality.

--- clothing/init.lua
+++ clothing/init.lua
@@ -1,4 +1,12 @@
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-    print(formname)
-    print(dump(fields))
-end)
+minetest.register_craftitem("clothing:test", {
+    decription = "Test cloth",
+    inventory_image = "clothing_test.png",
+    wield_image = "clothing_test.png",
+    stack_max = 1,
+
+    on_place = function(itemstack, placer, pointed_thing)
+        local skin = default.registered_player_models["character.x"].textures
+        skin[1] = skin[1] .. "^" .. "clothing_test.png"
+        default.player_set_textures(placer, skin)
+    end
+})
