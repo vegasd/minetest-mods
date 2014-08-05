960e361f2929dfd42479618973934a6d0cf11ac8
None <sullome@yandex.com>
2014-08-05 13:14:40 +0400

    Clear code. Ready for use.

--- clothing/init.lua
+++ clothing/init.lua
@@ -1,23 +1,6 @@
---{{{ Wear clothing (on_place)
-local function put_on(item, player)
-    local wname = item:get_name()
-    item = player:get_inventory():add_item("wear", item)
-    minetest.log("action",
-        player:get_player_name() ..
-        " puts on a " ..
-        wname
-    )
-    
-    default.player_set_textures(player, {generate_skin(player)})
-    minetest.log("action",
-        "update skin for player " ..
-        player:get_player_name()
-    )
-
-    return item
-end
-
-function generate_skin(player)
+clothing = {}
+--{{{ Wear clothing
+clothing.update_skin = function(player)
     local weared = player:get_inventory():get_list("wear")
     local skin = default.player_get_animation(player).textures[1]
     for _,itemstack in ipairs(weared) do
@@ -25,18 +8,18 @@ function generate_skin(player)
             skin = skin .. "^" .. itemstack:get_definition().wear_image
         end
     end
-    return skin
+    default.player_set_textures(player, {skin})
+    minetest.log("action",
+        "update skin for player " ..
+        player:get_player_name()
+    )
 end
 --}}}
 
---{{{ Save data
+--{{{ Save and restore data
 minetest.register_on_joinplayer(function(player)
-    default.player_set_textures(player, {generate_skin(player)})
+    clothing.update_skin(player)
 end)
-
---minetest.register_on_player_receive_fields(function(player, formname, fields)
---    print("DEBUG:", player, dump(formname), dump(fields))
---end)
 
 minetest.register_on_newplayer(function(player)
     -- Add inventory list for clothing
@@ -48,14 +31,11 @@ end)
 --{{{ Cloth
 -- Required values is:
 -- "wear_image" (this image is adding on player skin)
--- "on_place" (this is obvious)
 minetest.register_craftitem("clothing:test", {
     decription = "Test cloth",
     inventory_image = "clothing_test.png",
     wield_image = "clothing_test.png",
     wear_image = "clothing_test.png",
     stack_max = 1,
-
-    on_place = put_on
 })
 --}}}
