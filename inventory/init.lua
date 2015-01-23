550b85252bdad46ff2dcd5b979e099467abf28d4
None <sullome@yandex.com>
2015-01-24 01:52:04 +0300

    Learning structure of inventory. Start work.

--- inventory/init.lua
+++ inventory/init.lua
@@ -1,1 +1,16 @@
+minetest.register_chatcommand("inv_test", {
+    func = function()
+        -- Called when command is run.
+        -- Returns boolean success and text output.
+        local inv = minetest.get_inventory({type = "player", name = "singleplayer"})
+        print(dump(inv:get_lists()))
+    end
+})
 
+minetest.register_on_joinplayer(function(player)
+    local invref = player:get_inventory()
+    -- Something wrong
+    -- invref:set_lists({wear, main})
+    --
+    -- TODO: Set size and formspec
+end)
